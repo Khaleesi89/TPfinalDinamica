9541a99a625ba5139272eabfbee69fecd7be0d00
Gonzalo Parra <gonzzaparra@gmail.com>
2022-11-06 11:54:15 -0300

    Algo mas

--- Public/jsPuro/script.js
+++ Public/jsPuro/script.js
@@ -53,3 +53,47 @@ var swiper = new Swiper( ".books-slider", {
       },
     },
 });
+
+// Swiper
+var swiper = new Swiper( ".ingresos-slider", {
+  spaceBetween: 10,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
+  },
+});
+
+var swiper = new Swiper( ".reviews-slider", {
+  spaceBetween: 10,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    },
+    1024: {
+      slidesPerView: 3
+    },
+  },
+});
--- Views/home/newIndex.php
+++ Views/home/newIndex.php
@@ -93,10 +93,277 @@
         </div>
     </section>
 
+    <!-- Iconcitos fachas -->
+    <section class="icons-container">
+        <div class="icons">
+            <i class="fas fa-plane"></i>
+            <div class="content">
+                <h3>Envíos gratis a todo el país</h3>
+                <p>En pedidos mayores a $4000</p>
+            </div>
+        </div>
+        <div class="icons">
+            <i class="fas fa-lock"></i>
+            <div class="content">
+                <h3>Pagos seguros</h3>
+                <p>Tarjetas de crédito, débito y mercado pago</p>
+            </div>
+        </div>
+        <div class="icons">
+            <i class="fas fa-redo-alt"></i>
+            <div class="content">
+                <h3>Reembolsos</h3>
+                <p>Luego de haber recibido el producto</p>
+            </div>
+        </div>
+        <div class="icons">
+            <i class="fas fa-headset"></i>
+            <div class="content">
+                <h3>Atención 24/7</h3>
+                <p>Consúltenos en cualquier momento</p>
+            </div>
+        </div>
+    </section>
 
+    <section class="ingresos" id="ingresos">
+        <h1 class="heading"> <span>Nuevos Ingresos</span> </h1>
+        <div class="swiper ingresos-slider">
+            <div class="swiper-wrapper">
+                <a href="#" class="swiper-slide box">
+                    <div class="image">
+                        <img src="../../Public/img/libro-1.png" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>El Señor de los Anillos - Las Dos Torres</h3>
+                        <div class="price">$3599</div>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                    </div>
+                </a>
+                
+                <a href="#" class="swiper-slide box">
+                    <div class="image">
+                        <img src="../../Public/img/libro-2.png" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>Nuevo ingreso</h3>
+                        <div class="price">$2599</div>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </a>
 
+                <a href="#" class="swiper-slide box">
+                    <div class="image">
+                        <img src="../../Public/img/libro-3.png" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>Nuevo ingreso</h3>
+                        <div class="price">$2599</div>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star-half-alt"></i>
+                        </div>
+                    </div>
+                </a>
 
+                <a href="#" class="swiper-slide box">
+                    <div class="image">
+                        <img src="../../Public/img/libro-4.png" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>Nuevo ingreso</h3>
+                        <div class="price">$2599</div>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                    </div>
+                </a>
 
+                <a href="#" class="swiper-slide box">
+                    <div class="image">
+                        <img src="../../Public/img/libro-5.png" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>Nuevo ingreso</h3>
+                        <div class="price">$2599</div>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="#" class="swiper-slide box">
+                    <div class="image">
+                        <img src="../../Public/img/libro-6.png" alt="">
+                    </div>
+                    <div class="content">
+                        <h3>Nuevo ingreso</h3>
+                        <div class="price">$2599</div>
+                        <div class="stars">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <section class="oferta">
+        <div class="content">
+            <h3>Oferta del dia!</h3>
+            <h1>50% de descuento</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae velit ipsam dicta dignissimos odio iusto incidunt laborum ipsa, recusandae cumque!</p>
+            <a href="#" class="btn">Comprar ya</a>
+        </div>
+        <div class="image">
+            <img src="../../Public/img/oferta.jpg" alt="compralo wachin">
+        </div>
+    </section>
+
+    <section class="reviews" id="reviews">
+        <h1 class="heading"><span>Reviews de clientes</span></h1>
+        <div class="swiper reviews-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide box">
+                    <img src="../../Public/img/pic-1.png" alt="">
+                    <h3>John Salchichon</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ut!</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="../../Public/img/pic-2.png" alt="">
+                    <h3>John Salchichon</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ut!</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="../../Public/img/pic-3.png" alt="">
+                    <h3>John Salchichon</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ut!</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="../../Public/img/pic-4.png" alt="">
+                    <h3>John Salchichon</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ut!</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="../../Public/img/pic-6.png" alt="">
+                    <h3>John Salchichon</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ut!</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="../../Public/img/pic-5.png" alt="">
+                    <h3>John Salchichon</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, ut!</p>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="newsletter">
+        <form action="" method="">
+            <h3>Suscríbete para obtener las últimas novedades</h3>
+            <input type="email" name="mail" id="mail" placeholder="Ingrese su email" class="box">
+            <input type="submit" value="Suscribirse" class="btn">
+        </form>
+    </section>
+
+    <footer class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>Nuestras sucursales</h3>
+                <a href="#"><i class="fas fa-map-marker-alt"></i>La FAI</a>
+                <a href="#"><i class="fas fa-map-marker-alt"></i>La casa del masi</a>
+                <a href="#"><i class="fas fa-map-marker-alt"></i>La casa del jero</a>
+                <a href="#"><i class="fas fa-map-marker-alt"></i>La casa de mar</a>
+                <a href="#"><i class="fas fa-map-marker-alt"></i>La casa del gonza</a>
+            </div>
+            <div class="box">
+                <h3>Quick links</h3>
+                <a href="#"><i class="fas fa-arrow-right"></i>Home</a>
+                <a href="#"><i class="fas fa-arrow-right"></i>Ingresos</a>
+                <a href="#"><i class="fas fa-arrow-right"></i>Reviews</a>
+                <a href="#"><i class="fas fa-arrow-right"></i>Contacto</a>
+            </div>
+            <div class="box">
+                <h3>Extra links</h3>
+                <a href="#"><i class="fas fa-arrow-right"></i>Información de la cuenta</a>
+                <a href="#"><i class="fas fa-arrow-right"></i>Productos comprados</a>
+            </div>
+        </div>
+        <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+        </div>
+        <div class="credit">
+            <p>© Developed by Gonzalo Parra, Jerónimo Rojo, Marcia Klimisch & Maximiliano Ariel Hitter | 2022</p>
+        </div>
+    </footer>
 
 
 <!-- Swiper -->
