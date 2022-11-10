6573fa984b5a1a5bc147e2b3bbf726c1d53c8649
Marcia Klimisch <93682145+Khaleesi89@users.noreply.github.com>
2022-11-10 16:04:53 -0300

    asd

--- Models/testMenu.php
+++ Models/testMenu.php
@@ -5,13 +5,15 @@ require_once('../config.php');
 
 $menu = new Menu();
 var_dump ($menu);
-die();
+//die();
 
 //cargar
 
-$resultado = $menu->cargar(200, "Peluqueria","servicios que realiza","Servicios","09/12/1960");
-if($resultado){
+$menu->cargar(200, "Peluqueria","servicios que realiza",null,"09/12/1960");
+$resultado = $menu->insertar();
+if($resultado['respuesta']){
     echo "se realizo";
 }else{
+    var_dump( $resultado['errorInfo']);
     echo "juira bicho";
 }
