5cea7b445efdaf02462fd42836a3e540764104e9
Maximiliano Hitter <mr.vonhumbolt@gmail.com>
2022-11-18 20:51:05 -0300

    avances en vista de usuario con carga de rol

---
+++ Views/usuario/accion/guardar_roles.php
@@ -0,0 +1,3 @@
+<?php
+require_once('../../../config.php');
+$objUsuarioRolCon = new UsuarioRolController();
---
+++ Views/usuariorol/accion/usuariorol_listar.php
@@ -0,0 +1,17 @@
+<?php
+require_once('../../../config.php');
+$objUsuarioRolCon = new UsuarioRolController();
+$array = [];
+$lista = $objUsuarioRolCon->listarTodo($array);
+if(array_key_exists('arrayHTML', $lista)){
+    echo json_encode($lista['arrayHTML']);
+}else{
+    $arreglo_salid = array();
+    foreach ($lista as $key => $value) {
+    $nuevoElemen = $value->dameDatos();
+    array_push($arreglo_salid, $nuevoElemen);
+}
+}
+
+//var_dump($arreglo_salid);
+//echo json_encode($arreglo_salid);
