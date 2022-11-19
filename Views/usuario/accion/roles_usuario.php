46c1ab61936a63aa92afaa3c2dca77d1ece2633c
Maximiliano Hitter <mr.vonhumbolt@gmail.com>
2022-11-19 00:05:59 -0300

    intentos

--- Views/usuario/accion/roles_usuario.php
+++ Views/usuario/accion/roles_usuario.php
@@ -22,16 +22,38 @@ if(count($rta) != 0){
 }
 //var_dump($rolesTexto);
 //var_dump($rolesSimple);
+$string = "";
 $arrayOtro = [];
 if(count($rolesTexto) != 0){
     foreach ($rolesSimple as $id => $idrolArray) {
+        $valor = 'false';
         if(array_key_exists($id, $rolesTexto)){
             $rolesSimple[$id] = true;
+            $valor = 'true';
+            
         }
+        $arrayOtro["rol$id"] = $valor;
+        if($string == ''){
+            $string.="[$id => $valor,";
+        }else{
+            $string.= " $id => $valor,";
+        }
+        
     }
 }
+$string = substr($string, 0, -1);
+$string .= "] ";
+/* $string = substr($string, 0, -1);
+$string.= '}'; */
+
 //responder como _easyui_checkbox2 en adelante
 //var_dump($rolesSimple);
-
-
-echo json_encode($rolesSimple);
+/* $check = "_easyui_checkbox_";
+$contador = 2;
+foreach ($rolesSimple as $key => $value) {
+    $arrayOtro[$check.$contador] = $value;
+    $contador++;
+} */
+//echo $string;
+echo json_encode($string);
+//echo json_encode($arrayOtro);
--- Views/usuario/usuario_list.php
+++ Views/usuario/usuario_list.php
@@ -7,6 +7,7 @@ $lista = $objUsuCon->listarTodo();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,6 +20,7 @@ $lista = $objUsuCon->listarTodo();
     <script src="../../Vendor/jquery.easyui.min.js"></script>
     <title>Prueba isiUI</title>
 </head>
+
 <body>
     <table id="dg" title="Administrador de Usuarios" class="easyui-datagrid" style="width:700px;height:600px" url="accion/listar_usuario.php" toolbar="#toolbar" pagination="true" fitColumns="true" singleSelect="true">
         <thead>
@@ -34,149 +36,210 @@ $lista = $objUsuCon->listarTodo();
         <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-add" plain="true" onclick="newUsuario()">Nuevo Usuario</a>
         <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-edit" plain="true" onclick="editUsuario()">Editar Usuario</a>
         <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onclick="destroyUsuario()">Deshabilitar Usuario</a>
-        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onclick="newRol()">Ver Roles</a>  
+        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-remove" plain="true" onclick="newRol()">Ver Roles</a>
     </div>
     <div id="dlg" class="easyui-dialog" style="width:600px;" data-options="closed:true,modal:true,border:'thin',buttons:'#dlg-buttons'">
-    <form id="fm" method="POST" novalidate style="margin:0,padding:20px 50px;">
-    <h3>Usuario Información</h3>
-    <!-- <div style="margin-bottom:10px;">
+        <form id="fm" method="POST" novalidate style="margin:0,padding:20px 50px;">
+            <h3>Usuario Información</h3>
+            <!-- <div style="margin-bottom:10px;">
         <input name="idusuario" id="idusuario" class="easyui-textbox" required="true" label="Id usuario" style="width:100%;">
     </div> -->
-    <div style="margin-bottom:10px;">
-        <input name="usnombre" id="usnombre" class="easyui-textbox" required="true" label="Nombre" style="width:100%;">
-    </div>
-    <div style="margin-bottom:10px;">
-        <input name="uspass" id="uspass" class="easyui-textbox" required="true" label="Password" style="width:100%;">
-    </div>
-    <div style="margin-bottom:10px;">
-        <input name="usmail" id="usmail" class="easyui-textbox" required="true" label="Email" style="width:100%;">
-    </div>
-        
-        
-    </form>
-    <div id="dlg-buttons">
-        <a href="javascript:void(0)" class="easyui-button c6" iconCls="icon-ok" onclick="guardarUsuario()" style="width:90px">Aceptar</a>
-        <a href="javascript:void(0)" class="easyui-button" iconCls="icon-cancel" onclick="javascript:$('#dlg').dialog('close')" style="width:90px">Cancelar</a>
-    </div>
-    <!--MODAL FORMULARIO DE ROLES--> 
-    <div id="dlg1" class="easyui-dialog" style="width:600px;" data-options="closed:true,modal:true,border:'thin',buttons:'#dlg1-buttons'">
-    <form id="fm1" method="POST" novalidate style="margin:0,padding:20px 50px;">
-    <h3>Información de Roles</h3>
-    <!-- <div style="margin-bottom:10px;">
+            <div style="margin-bottom:10px;">
+                <input name="usnombre" id="usnombre" class="easyui-textbox" required="true" label="Nombre" style="width:100%;">
+            </div>
+            <div style="margin-bottom:10px;">
+                <input name="uspass" id="uspass" class="easyui-textbox" required="true" label="Password" style="width:100%;">
+            </div>
+            <div style="margin-bottom:10px;">
+                <input name="usmail" id="usmail" class="easyui-textbox" required="true" label="Email" style="width:100%;">
+            </div>
+
+
+        </form>
+        <div id="dlg-buttons">
+            <a href="javascript:void(0)" class="easyui-button c6" iconCls="icon-ok" onclick="guardarUsuario()" style="width:90px">Aceptar</a>
+            <a href="javascript:void(0)" class="easyui-button" iconCls="icon-cancel" onclick="javascript:$('#dlg').dialog('close')" style="width:90px">Cancelar</a>
+        </div>
+        <!--MODAL FORMULARIO DE ROLES-->
+        <div id="dlg1" class="easyui-dialog" style="width:600px;" data-options="closed:true,modal:true,border:'thin',buttons:'#dlg1-buttons'">
+            <form id="fm1" method="POST" novalidate style="margin:0,padding:20px 50px;" enctype="multipart/form-data">
+                <h3>Información de Roles</h3>
+                <!-- <div style="margin-bottom:10px;">
         <input name="idusuario" id="idusuario" class="easyui-textbox" required="true" label="Id usuario" style="width:100%;">
     </div> 
 
     ARMAR LA RESPUESTA DE LOS ROLES-->
-
-    <?php
-        foreach ($arrayRoles as $key => $value) {
-            $texto = $value->dameDatos();
-            $id = $texto['idrol'];
-            $rodescripcion = $texto['rodescripcion'];
-            echo "<div style=\"margin-bottom:10px;\">
-            <input name=\"$id\" id=\"$id\" class=\"easyui-checkbox\" required=\"false\" label=\"$rodescripcion\" style=\"width:2%;\">
+                <?php
+                $stringArr = "<script>let arrayF = [";
+                foreach ($arrayRoles as $key => $value) {
+                    $texto = $value->dameDatos();
+                    $id = $texto['idrol'];
+                    $rodescripcion = $texto['rodescripcion'];
+                    $value = 'true';
+                    echo "<div style=\"margin-bottom:20px\">
+            <label for=\"$id\" class=\"textbox-label\">$rodescripcion:</label>
+            <input id=rol$id type=\"checkbox\" name=\"$id\">
         </div>";
-        }
-    ?>
-        
-        
-    </form>
-    <div id="dlg1-buttons">
-        <a href="javascript:void(0)" class="easyui-button c6" iconCls="icon-ok" onclick="guardarRoles()" style="width:90px">Aceptar</a>
-        <a href="javascript:void(0)" class="easyui-button" iconCls="icon-cancel" onclick="javascript:$('#dlg1').dialog('close')" style="width:90px">Cancelar</a>
-    </div>
+                    $stringArr.="'rol$id',";
+                }
+                $stringArr = substr($stringArr, 0, -1);
+                $stringArr .= '];</script>';
+                echo $stringArr;
+                ?>
 
 
-    <script>
-        var url;
-        function newUsuario(){
-            $('#dlg').dialog('open').dialog('center').dialog('setTitle', 'Nuevo usuario');
-            $('#fm').form('clear');
-            url='accion/insertar_usuario.php';
-        }
-        function newRol(){
-            var row = $('#dg').datagrid('getSelected');
-            if(row){
-                $('#dlg1').dialog('open').dialog('center').dialog('setTitle', 'Roles');
-                $('#fm1').form('clear');
-                $('#fm1').form('load', 'accion/roles_usuario?idusuario='+row.idusuario);
-                
-            }
-            
-    
-        }
-        function guardarRoles(){
-            $('#fm1').form('submit', {
-                url:'accion/guardar_roles.php',
-                onSubmit:function(){
-                    return $(this).form('validate');
-                },
-                success:function(result){
-                    var result=eval('('+result+')');
-                    //alert('Volvio servidor');
-                    if(!result.respuesta){
-                        $.messager.show({
-                            title:'Error',
-                            msg:result.errorMsg
+            </form>
+            <div id="dlg1-buttons">
+                <a href="javascript:void(0)" class="easyui-button c6" iconCls="icon-ok" onclick="guardarRoles()" style="width:90px">Aceptar</a>
+                <a href="javascript:void(0)" class="easyui-button" iconCls="icon-cancel" onclick="javascript:$('#dlg1').dialog('close')" style="width:90px">Cancelar</a>
+            </div>
+
+
+            <script>
+                var url;
+                var datos;
+                var arralgo;
+
+                function newUsuario() {
+                    $('#dlg').dialog('open').dialog('center').dialog('setTitle', 'Nuevo usuario');
+                    $('#fm').form('clear');
+                    url = 'accion/insertar_usuario.php';
+                }
+
+                function newRol() {
+                    var row = $('#dg').datagrid('getSelected');
+                    if (row) {
+                        $('#dlg1').dialog('open').dialog('center').dialog('setTitle', 'Roles');
+                        $('#fm1').form('clear');
+                        //asd = new XMLHttpRequest('POST', 'accion/roles_usuario?idusuario='+row.idusuario);
+                        $('#fm1').form('load', 'accion/roles_usuario?idusuario=' + row.idusuario);
+                        //data = {asd:'a'};
+                        datos = fetch('accion/roles_usuario?idusuario=' + row.idusuario, {
+                            method: "POST",
+                            body: JSON.stringify(datos),
+                            headers: {
+                                "Content-Type": "application/json"
+                            }
+                        }).then((e) => {
+                            return e.json();
+                        }).then(data => {
+                            datos = data;
                         });
-                    }else{
-                        $('#dlg1').dialog('close');
+                        //console.table(JSON.stringify(datos));
+                        for (const key in datos) {
+                            if (datos.hasOwnProperty.call(datos, key)) {
+                                const element = datos[key];
+                                console.log(element);
+                            }
+                        }
+                        let algo = arrayF.map(element => {
+                            result = datos.element;
+                            if(result){
+                                document.getElementById(`${element}`).click();
+                            }
+                        })
+                        /* for (const key in datos) {
+                            if (Object.hasOwnProperty.call(datos, key)) {
+                                const element = datos[`${key}`];
+                                if(element){
+                                    document.getElementById(`${element}`).click();
+                                }
+                            }
+                        } */
+                        /* datos = Array.from(datos)
+                        arrayF.forEach(element => {
+                            result = element in datos;
+                             if(result){
+                                document.getElementById(element).click();
+                            } 
+                            
+                        }); */
                         
                     }
+
                 }
-            })
-        }
-        function editUsuario(){
-            var row = $('#dg').datagrid('getSelected');
-            if(row){
-                $('#dlg').dialog('open').dialog('center').dialog('setTitle', 'Editar usuario');
-                $('#fm').form('load', row);
-                url='accion/edit_usuario.php?idusuario='+row.idusuario;
-            }
-        }
-        function guardarUsuario(){
-            $('#fm').form('submit', {
-                url:url,
-                onSubmit:function(){
-                    return $(this).form('validate');
-                },
-                success:function(result){
-                    var result=eval('('+result+')');
-                    //alert('Volvio servidor');
-                    if(!result.respuesta){
-                        $.messager.show({
-                            title:'Error',
-                            msg:result.errorMsg
-                        });
-                    }else{
-                        $('#dlg').dialog('close');
-                        $('#dg').datagrid('reload');
-                    }
-                }
-            })
-        }
-        function destroyUsuario(){
-            var row=$('#dg').datagrid('getSelected');
-            if(row){
-                $.messager.confirm('confirm', 'Seguro desea eliminar el usuario?', function(r){
-                    if(r){
-                        $.post('accion/destroy_usuario.php?idusuario='+row.idusuario,{idusuario:row.id}, function(result){
-                            alert('Volvio servidor');
-                            if(result.respuesta){
-                                $('#dg').datagrid('reload');
-                            }else{
+
+
+                
+
+                function guardarRoles() {
+                    $('#fm1').form('submit', {
+                        url: 'accion/guardar_roles.php',
+                        onSubmit: function() {
+                            return $(this).form('validate');
+                        },
+                        success: function(result) {
+                            var result = eval('(' + result + ')');
+                            //alert('Volvio servidor');
+                            if (!result.respuesta) {
                                 $.messager.show({
-                                    title:'Error',
-                                    msg:result.errorMsg
+                                    title: 'Error',
+                                    msg: result.errorMsg
                                 });
+                            } else {
+                                $('#dlg1').dialog('close');
+
                             }
-                        }, 'json');
+                        }
+                    })
+                }
+
+                function editUsuario() {
+                    var row = $('#dg').datagrid('getSelected');
+                    if (row) {
+                        $('#dlg').dialog('open').dialog('center').dialog('setTitle', 'Editar usuario');
+                        $('#fm').form('load', row);
+                        url = 'accion/edit_usuario.php?idusuario=' + row.idusuario;
                     }
-                })
-            }
-        }
-    </script>
-        
-    </div>
+                }
+
+                function guardarUsuario() {
+                    $('#fm').form('submit', {
+                        url: url,
+                        onSubmit: function() {
+                            return $(this).form('validate');
+                        },
+                        success: function(result) {
+                            var result = eval('(' + result + ')');
+                            //alert('Volvio servidor');
+                            if (!result.respuesta) {
+                                $.messager.show({
+                                    title: 'Error',
+                                    msg: result.errorMsg
+                                });
+                            } else {
+                                $('#dlg').dialog('close');
+                                $('#dg').datagrid('reload');
+                            }
+                        }
+                    })
+                }
+
+                function destroyUsuario() {
+                    var row = $('#dg').datagrid('getSelected');
+                    if (row) {
+                        $.messager.confirm('confirm', 'Seguro desea eliminar el usuario?', function(r) {
+                            if (r) {
+                                $.post('accion/destroy_usuario.php?idusuario=' + row.idusuario, {
+                                    idusuario: row.id
+                                }, function(result) {
+                                    alert('Volvio servidor');
+                                    if (result.respuesta) {
+                                        $('#dg').datagrid('reload');
+                                    } else {
+                                        $.messager.show({
+                                            title: 'Error',
+                                            msg: result.errorMsg
+                                        });
+                                    }
+                                }, 'json');
+                            }
+                        })
+                    }
+                }
+            </script>
+
+        </div>
 </body>
+
 </html>
